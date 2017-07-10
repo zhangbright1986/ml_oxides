67b6efaa53550b6ea92cff2da52d3dbd738f39c4
bright <zhangbright1986@gmail.com>
2017-07-10 16:30:14 -0400

    remodeled

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mloxides',
+      version='1.0',
+      description='ML for Oxides',
+      author='Liang Zhang',
+      author_email='zhangbright1986@gmail.com',
+      packages=['prepare', 'template.data'],
+      )
